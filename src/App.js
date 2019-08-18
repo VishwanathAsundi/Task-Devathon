36c20b6b88e8c6cb350ef9a2f634fb9c833654f7
vishwanath <vishwanathasundi1997@gmail.com>
2019-08-18 14:23:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,140 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PopUp from './popUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  state = {
+    currentProduct: {},
+    showPopUp: false
+  }
+  showPopUp = (product) => {
+    this.setState({ currentProduct: product, showPopUp: true });
+  }
+  closePopup = () => {
+    this.setState({ showPopUp: false });
+  }
+
+
+  render() {
+    const productList = [
+      {
+        src: "assets/images/emma3.jpg",
+        productTitle: "scarps",
+        price: "500 $",
+        id: 1
+      },
+      {
+        src: "assets/images/emma4.jpg",
+        productTitle: "Hat",
+        price: "700 $",
+        id: 2
+      },
+      {
+        src: "assets/images/emma5.jpg",
+        productTitle: "Sweater",
+        price: "900 $",
+        id: 3
+      },
+      {
+        src: "assets/images/emma6.jpg",
+        productTitle: "Casuals",
+        price: "100 $",
+        id: 4
+      },
+      {
+        src: "assets/images/emma2.jpg",
+        productTitle: "Modern",
+        price: "300 $",
+        id: 5
+      },
+      {
+        src: "assets/images/emma1.jpg",
+        productTitle: "scarps",
+        price: "150 $",
+        id: 6
+      },
+      {
+        src: "assets/images/emma3.jpg",
+        productTitle: "scarps",
+        price: "500 $",
+        id: 7
+      },
+      {
+        src: "assets/images/emma4.jpg",
+        productTitle: "scarps",
+        price: "700 $",
+        id: 8
+      },
+      {
+        src: "assets/images/emma5.jpg",
+        productTitle: "scarps",
+        price: "900 $",
+        id: 9
+      },
+      {
+        src: "assets/images/emma6.jpg",
+        productTitle: "scarps",
+        price: "100 $",
+        id: 10
+      },
+
+    ]
+    return (
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-2">
+            <div class="left-menu p-5">
+              <h5 class="set-opacity">Left menu</h5>
+            </div>
+          </div>
+          <div class="col-md-10">
+            <section class="top-header p-5 text-center">
+              <h5 class="set-opacity">Header goes here</h5>
+            </section>
+            <section class="container bg-light p-5">
+              <div class="header-text text-center">
+                <h4>Add details for your products</h4>
+                <p class="set-opacity ">Unpriced product will be hidden. You can edit them later.</p>
+              </div>
+              <div class="row">
+                {
+                  productList.map((product, i) => {
+                    return (
+                      <div class="col-md-4" key={i}>
+                        <div class="product" onClick={(e) => this.showPopUp(product)}
+                        >
+                          <img src={product.src} alt="" />
+                          <div class="product-descr">
+                            <h6 class="title set-opacity">Product Title: {product.productTitle}</h6>
+                            <span class="price set-opacity">{product.price}</span>
+                          </div>
+                          {
+                            this.state.showPopUp && product.id === this.state.currentProduct.id ?
+                              <PopUp
+                                product={this.state.currentProduct}
+                              //useOutsideAlerter={this.useOutsideAlerter}
+                              />
+                              : ''
+                          }
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+
+
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
---
+++ src/popUp.js
@@ -0,0 +1,85 @@
+import React, { useRef, useEffect } from 'react';
+
+function useOutsideAlerter(ref) {
+    function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+            document.querySelector(".popup").style.display = "none";
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    });
+}
+
+const PopUp = (props) => {
+    const { product } = props;
+    console.log(product);
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef);
+    return (
+        <div class="popup border bounceIn bg-white" ref={wrapperRef}>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="product-img">
+                        <img src={product.src} alt="" />
+                    </div>
+                    <div class="product-details pt-3">
+                        <form>
+                            <div class="border-bottom ">
+                                <label class="set-opacity mb-0">Product Title</label>
+                                <input type="text" class="border-0 custom-input"
+                                    value={product.productTitle} />
+                            </div>
+                            <div class="border-bottom mt-2">
+                                <label class="set-opacity mb-0">Price</label><br />
+                                <input type="text" class="border-0 custom-input"
+                                    value={product.price} />
+                            </div>
+                            <div class="border-bottom mt-2">
+                                <label class="set-opacity mb-0">Offer Price</label>
+                                <input type="text" class="border-0 custom-input"
+                                    placeholder="Enter Offer Price" />
+                            </div>
+                            <div class="border-bottom mt-2">
+                                <label class="set-opacity mb-0">Shipping Cost</label>
+                                <input type="number" class="border-0 custom-input" value="0" />
+                            </div>
+                            <div class="border-bottom mt-2">
+                                <label class="set-opacity mb-0">Inventory</label>
+                                <input type="number" class="border-0 custom-input" value="0" />
+                            </div>
+                            <div class="border-bottom mt-2">
+                                <label class="set-opacity mb-0">Description</label>
+                                <input type="text" class="border-0 custom-input"
+                                    placeholder="Enter Description for Profuct" />
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6  bg-white border border-top-0 border-bottom-0">
+                    <div class="add-img text-center">
+                        <div class="img-box">
+                            <img src="assets/images/add-pic.jpg" alt="" />
+                            <p class="set-opacity">Add More Photos</p>
+                        </div>
+
+                    </div>
+                    <div class="variations">
+                        <p class="plus">+</p>
+                        <p class="set-opacity">Have variations to your product like Size,
+                                                    Color and more?</p>
+                        <button class="btn btn-secondary btn-block next-btn">
+                            <i class="fas fa-arrow-right"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default PopUp;
